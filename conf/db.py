9dae820bbe18b136b28096d95d3aa21c9c797fdf
Sergii Prostov <spro77@gmail.com>
2025-07-12 15:51:38 -0600

    initialize project with Docker, FastAPI, and PostgreSQL setup

---
+++ conf/db.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+# Use environment variable if available, otherwise use localhost
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:567234@localhost:5432/hw02")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
