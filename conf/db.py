df6de989b40095f8db382dd0ef7824a23c968603
Sergii Prostov <spro77@gmail.com>
2025-07-13 09:47:31 -0600

    update database connection URL to use Docker

--- conf/db.py
+++ conf/db.py
@@ -2,8 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-# Use environment variable if available, otherwise use localhost
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:567234@localhost:5432/hw02")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:567234@db:5432/hw02")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
